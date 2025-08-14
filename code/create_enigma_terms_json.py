07090f4310e63c91baf4a96344092c3b8a89892d
Alyssa Dai <alyssa.ydai@gmail.com>
2025-08-14 16:04:47 -0400

    run pre-commit on script

--- code/create_enigma_terms_json.py
+++ code/create_enigma_terms_json.py
@@ -1,21 +1,24 @@
+import json
+import os
+import sys
+from pathlib import Path
+
 import gspread
 import pandas as pd
 import pandera.pandas as pa
-import sys
-import json
-from pathlib import Path
-import os
 
 GOOGLE_API_KEY = os.environ.get("COMMUNITIES_GOOGLE_API_KEY")
 VOCAB_GSHEET_ID = os.environ.get("ENIGMA_ASSESSMENTS_GSHEET_ID")
-OUTPUT_FILEPATH = Path(__file__).absolute().parents[1] / "configs/ENIGMA-PD/assessment.json"
+OUTPUT_FILEPATH = (
+    Path(__file__).absolute().parents[1] / "configs/ENIGMA-PD/assessment.json"
+)
 
 # TODO: Do we want to increment the version each time the config file is regenerated?
 VOCAB_METADATA = {
     "namespace_prefix": "enigmapd",
     "namespace_url": "https://enigma.ini.usc.edu/ongoing/enigma-parkinsons/vocab/assessments/",
     "vocabulary_name": "ENIGMA-PD vocabulary of assessment terms",
-    "version": "1.0.0"
+    "version": "1.0.0",
 }
 
 gc = gspread.api_key(GOOGLE_API_KEY)
@@ -25,15 +28,17 @@ vocab_file_schema = pa.DataFrameSchema(
     # 'id', 'name', 'abbreviation', and 'description' are required columns,
     # but only 'id' and 'name' must not have any missing values
     {
-        "id": pa.Column(str, checks=pa.Check.str_startswith("trm_"), nullable=False),
+        "id": pa.Column(
+            str, checks=pa.Check.str_startswith("trm_"), nullable=False
+        ),
         "name": pa.Column(str, nullable=False),
         "abbreviation": pa.Column(str, nullable=True),
         "description": pa.Column(str, nullable=True),
         "same_as": pa.Column(str, nullable=True, required=False),
-        "status": pa.Column(str, nullable=True, required=False)
+        "status": pa.Column(str, nullable=True, required=False),
     },
     # Only validate and pass to the result the columns defined in the schema
-    strict="filter"
+    strict="filter",
 )
 
 
@@ -56,7 +61,9 @@ def lowercase_column_names(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def create_terms_json(vocab_table: pd.DataFrame, vocab_metadata: dict) -> list[dict]:
+def create_terms_json(
+    vocab_table: pd.DataFrame, vocab_metadata: dict
+) -> list[dict]:
     vocab_records = vocab_table.to_dict(orient="records")
     return [{**vocab_metadata, "terms": vocab_records}]
 
