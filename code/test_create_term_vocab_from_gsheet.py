13a96aa01f9db80a3adaf0b8b0ccebd49f137dd7
Alyssa Dai <alyssa.ydai@gmail.com>
2026-02-04 23:03:31 -0500

    assert over both valid and invalid term IDs

--- code/test_create_term_vocab_from_gsheet.py
+++ code/test_create_term_vocab_from_gsheet.py
@@ -6,26 +6,33 @@ from create_term_vocab_from_gsheet import vocab_file_schema
 
 def test_invalid_term_ids_fail_validation():
     """Test that term IDs containing invalid characters fail schema validation."""
-    invalid_vocab_table = {
-        "id": ["TERM1", "TERM2!", "TERM 3"],
-        "name": ["Term 1", "Term 2", "Term 3"],
-        "abbreviation": ["T1", "T2", "T3"],
-        "description": [
-            "Term 1 description",
-            "Term 2 description",
-            "Term 3 description",
-        ],
-    }
-    expected_error_substrs = [
-        "must only contain alphanumeric characters, underscores, and hyphens",
-        "TERM2!",
-        "TERM 3",
-    ]
-
-    df = pd.DataFrame(invalid_vocab_table)
+    invalid_vocab_table = pd.DataFrame(
+        {
+            "id": ["TERM-1", "TERM_2", "TERM2!", "TERM 3"],
+            "name": [
+                "Valid Term 1",
+                "Valid Term 2",
+                "Invalid Term 3",
+                "Invalid Term 4",
+            ],
+            "abbreviation": ["T1", "T2", "T3", "T4"],
+            "description": [
+                "Term 1 description",
+                "Term 2 description",
+                "Term 3 description",
+                "Term 4 description",
+            ],
+        }
+    )
+    expected_error = "must only contain alphanumeric characters"
+    valid_ids = ["TERM-1", "TERM_2"]
+    invalid_ids = ["TERM2!", "TERM 3"]
 
     with pytest.raises(pa.errors.SchemaError) as exc_info:
-        vocab_file_schema.validate(df)
+        vocab_file_schema.validate(invalid_vocab_table)
 
-    for substr in expected_error_substrs:
-        assert substr in str(exc_info.value)
+    assert expected_error in str(exc_info.value)
+    for valid_id in valid_ids:
+        assert valid_id not in str(exc_info.value)
+    for invalid_id in invalid_ids:
+        assert invalid_id in str(exc_info.value)
